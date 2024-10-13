416fd02678bfd3094d9293c81731f747bb0d17c8
Kate McGill <kateamcgill@gmail.com>
2024-10-13 16:46:54 -0600

    Init repo files

---
+++ setup.R
@@ -0,0 +1,23 @@
+#setup script
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'lterdatasampler',
+              'sf',
+              'terra',
+              'tigris',
+              'rmarkdown',
+              'elevatr')
+
+packageLoad(packages)
